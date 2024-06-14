b7a67723519f0765c65f28a053eb672658b9c1f2
nhientruong04 <truonghaonhien2822004@gmail.com>
2024-06-14 15:31:52 +0700

    medium: min stack, this is quite a clever approach. Thought impossible since min heap requires logn for basic operations

---
+++ algo_medium/min_stack.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if len(self.stack) == 0:
+            self.stack.append((val, val))
+        else:
+            min_val = self.stack[-1][1] if val >= self.stack[-1][1] else val
+            self.stack.append((val, min_val))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
